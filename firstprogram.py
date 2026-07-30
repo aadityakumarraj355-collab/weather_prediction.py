c9f6e517a21c9e0a9b54a025ff66d82810d83afa
aadityakumarraj355-collab <aadityakumarraj355@gmail.com>
2026-07-30 10:22:45 +0530

    Delete firstprogram.py

--- firstprogram.py
+++
@@ -1,9 +0,0 @@
-'''print("Hello Aaditya")
-print("My Name Is Aaditya Kumar"," & My Age Is 22")
-print("My Age Is 22")
-age =22'''
-age= 22
-name="Aaditya"
-Religion= "Hindu"
-print("My name is:",name,)
-print("My age is:",age," &","My Religion is: ",Religion)
