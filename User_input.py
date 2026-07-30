c2685bf2fef355c293b177fce10d77da6130433f
aadityakumarraj355-collab <aadityakumarraj355@gmail.com>
2026-07-30 10:23:18 +0530

    Delete User_input.py

--- User_input.py
+++
@@ -1,8 +0,0 @@
-name =input("Enter your Name: ")
-print("Welcome",name)
-# user input is always a string
-a = input("the number: ")
-print(type(a))
-#Here th output is string but we want the number as integer so, to get the number as integer we use tye casting method
-b= int(input("the number: "))
-print(type(b))
