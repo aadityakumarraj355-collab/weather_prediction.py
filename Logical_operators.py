9aed7f5b45f87c3e67afa3699b9c43d428b22130
aadityakumarraj355-collab <aadityakumarraj355@gmail.com>
2026-07-30 10:19:56 +0530

    Delete Logical_operators.py

--- Logical_operators.py
+++
@@ -1,8 +0,0 @@
-a=20
-b=30
-print(not(b>a)) # answer is true but h ouput is false because not operator gives the opposite boolean answer
-val1 = True
-val2 = True
-print("and operatoe:",val1 and val2) # both val1 and val2 are True so the outpu is True
-
-print("or opertaor :", val1 or val2)
